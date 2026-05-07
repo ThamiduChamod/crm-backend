ca54ef9b7e4a7fa3a9a4e88840a4e102750e2ccb
ThamiduChamod <thamiduchamod748@gmail.com>
2026-05-08 00:39:40 +0530

    implement leader  register

---
+++ src/controllers/leader.controller.ts
@@ -0,0 +1,48 @@
+import { db } from "../config/db.js";
+import { AuthRequest } from "../middleware/auth.js";
+import { Response } from "express";
+
+export const saveLeader = async (req: AuthRequest, res: Response) => {
+    console.log("method called");
+    console.log(req.body);
+    const { name, company, email, phone, source, status, dealValue } = req.body;
+    // console.log(name, company, email, phone, source, status, dealValue);
+    if(!name || !company || !email || !phone || !source || !status || !dealValue) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    
+    console.log("User ID:", req.user?.sub);
+    if(!req.user) {
+        console.log("User is not authenticated");
+        return res.status(401).json({ message: "Unauthorized" });
+
+    }
+    console.log("User is authenticated");
+    try {
+        const existingLeader = await db.leader.findUnique({
+            where:{ email }
+        })
+
+        if(existingLeader){
+            return res.status(405).json({message: "Leader are already existing"})
+        }
+
+        const newLeader = await db.leader.create({
+            data: {
+                name,
+                company,
+                email,
+                phone,
+                source,
+                status,
+                dealValue: Number(dealValue),
+                assignedUserId: Number(req.user.sub)
+            }
+        });
+
+        res.status(200).json({isAdded: true, message: "Leader saved successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
---
+++ src/routes/leader.routes.ts
@@ -0,0 +1,15 @@
+import Router from "express";
+import { authenticate } from "../middleware/auth.js";
+import { requireRole } from "../middleware/role.js";
+import { saveLeader } from "../controllers/leader.controller.js";
+
+const router = Router();
+// router.post("/save", saveLeader);
+router.post("/save", authenticate, requireRole(["USER"]), saveLeader);
+// router.get("/list", authenticate, requireRole(["USER"]), listLeaders);
+// router.get("/details/:id", authenticate, requireRole(["USER"]), getLeaderDetails);
+// router.put("/update/:id", authenticate, requireRole(["USER"]), updateLeader);
+// router.delete("/delete/:id", authenticate, requireRole(["USER"]), deleteLeader);
+
+
+export default router;
