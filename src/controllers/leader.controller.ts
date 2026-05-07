3507ba35100df8ba8e7b5819dfee6383466c614e
ThamiduChamod <thamiduchamod748@gmail.com>
2026-05-08 03:51:47 +0530

    implement  update leader

--- src/controllers/leader.controller.ts
+++ src/controllers/leader.controller.ts
@@ -1,3 +1,4 @@
+import { isUint16Array } from "node:util/types";
 import { db } from "../config/db.js";
 import { AuthRequest } from "../middleware/auth.js";
 import { Response } from "express";
@@ -100,6 +101,38 @@ export const deleteLeader = async (req: AuthRequest, res: Response) => {
             }
         });
         res.status(200).json({ message: "Leader deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const updateLeader = async (req: AuthRequest, res: Response) => {
+    if(!req.user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    const { name, email, status, dealValue } = req.body;
+    try {
+        const leader = await db.leader.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        if(!leader) {
+            return res.status(404).json({ message: "Leader not found" });
+        }
+        const updatedLeader = await db.leader.update({
+            where: {
+                id: Number(req.params.id)
+            },
+            data: {
+                name,
+                email,
+                status,
+                dealValue: Number(dealValue)
+            }
+        });
+        res.status(200).json({isUpdated: true, message: "Leader updated successfully", data: { leader: updatedLeader } });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server error" });
--- src/routes/leader.routes.ts
+++ src/routes/leader.routes.ts
@@ -1,14 +1,14 @@
 import Router from "express";
 import { authenticate } from "../middleware/auth.js";
 import { requireRole } from "../middleware/role.js";
-import { saveLeader, getAll, getLeaderDetails, deleteLeader } from "../controllers/leader.controller.js";
+import { saveLeader, getAll, getLeaderDetails, deleteLeader, updateLeader } from "../controllers/leader.controller.js";
 
 const router = Router();
 // router.post("/save", saveLeader);
 router.post("/save", authenticate, requireRole(["USER"]), saveLeader);
 router.get("/all", authenticate, requireRole(["USER"]), getAll);
 router.get("/details/:id", authenticate, requireRole(["USER"]), getLeaderDetails);
-// router.put("/update/:id", authenticate, requireRole(["USER"]), updateLeader);
+router.put("/update/:id", authenticate, requireRole(["USER"]), updateLeader);
 router.delete("/delete/:id", authenticate, requireRole(["USER"]), deleteLeader);
 
 
